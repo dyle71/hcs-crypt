adebbb76bc81f89b2cddf676ea6089cf0e47b6d7
Oliver Maurhart <oliver.maurhart@headcode.space>
2020-11-18 10:20:39 +0100

    Adds shared test data

---
+++ test/shared/ipsum_lorem.hpp
@@ -0,0 +1,77 @@
+/*
+ * This file is part of the headcode.space mem.
+ *
+ * The 'LICENSE.txt' file in the project root holds the software license.
+ * Copyright (C) 2020 headcode.space
+ * https://www.headcode.space, <info@headcode.space>
+ */
+
+#ifndef HEADCODE_SPACE_CRYPT_TEST_SHARED_IPSUM_LOREM_HPP
+#define HEADCODE_SPACE_CRYPT_TEST_SHARED_IPSUM_LOREM_HPP
+
+#include <string>
+
+
+std::string const IPSUM_LOREM_TEXT{
+        "\n\
+Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod \n\
+tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \n\
+quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi \n\
+consequat. Quis aute iure reprehenderit in voluptate velit esse cillum \n\
+dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non \n\
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\
+\n\
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse \n\
+molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero \n\
+eros et accumsan et iusto odio dignissim qui blandit praesent luptatum \n\
+zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum \n\
+dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh \n\
+euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.\n\
+\n\
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper \n\
+suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel \n\
+eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, \n\
+vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et \n\
+iusto odio dignissim qui blandit praesent luptatum zzril delenit augue \n\
+duis dolore te feugait nulla facilisi.\n\
+\n\
+Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet \n\
+doming id quod mazim placerat facer possim assum. Lorem ipsum dolor \n\
+sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod \n\
+tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad \n\
+minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis \n\
+nisl ut aliquip ex ea commodo consequat.\n\
+\n\
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse \n\
+molestie consequat, vel illum dolore eu feugiat nulla facilisis.\n\
+\n\
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita \n\
+kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit \n\
+amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam \n\
+nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, \n\
+sed diam voluptua. At vero eos et accusam et justo duo dolores et ea \n\
+rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum \n\
+dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, \n\
+At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et \n\
+nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, \n\
+kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem \n\
+ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing \n\
+elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna \n\
+aliquyam erat.\n\
+\n\
+Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut \n\
+labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et \n\
+accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no \n\
+sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit \n\
+amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt \n\
+ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos \n\
+et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, \n\
+no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor \n\
+sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor \n\
+invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At \n\
+vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd \n\
+gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\n\
+"};
+
+
+#endif
