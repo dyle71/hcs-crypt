9d64a2c71a6edd87827ac78ee1e725da8239eb2d
Oliver Maurhart <oliver.maurhart@headcode.space>
2020-11-21 13:08:40 +0100

    Adds benchmark tests

--- test/shared/ipsum_lorem.hpp
+++ test/shared/ipsum_lorem.hpp
@@ -1,5 +1,5 @@
 /*
- * This file is part of the headcode.space mem.
+ * This file is part of the headcode.space crypt.
  *
  * The 'LICENSE.txt' file in the project root holds the software license.
  * Copyright (C) 2020 headcode.space
