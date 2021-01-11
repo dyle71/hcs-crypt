722204f98406d7dc19e8ac7301a96868e655cf29
Oliver Maurhart <oliver.maurhart@headcode.space>
2021-01-11 09:33:38 +0100

    Adds benchmark tests for OpenSSL SHA1, SHA224, SHA256, SHA384 and SHA512

---
+++ test/benchmark/openssl/test_openssl_sha1.cpp
@@ -0,0 +1,70 @@
+/*
+ * This file is part of the headcode.space crypt.
+ *
+ * The 'LICENSE.txt' file in the project root holds the software license.
+ * Copyright (C) 2020 headcode.space
+ * https://www.headcode.space, <info@headcode.space>
+ */
+
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <headcode/benchmark/benchmark.hpp>
+#include <headcode/mem/mem.hpp>
+#include <headcode/crypt/crypt.hpp>
+
+#include <shared/ipsum_lorem.hpp>
+
+
+TEST(BenchmarkOPENSSLSHA1, OPENSSLSHA1StdString1000) {
+
+    auto loop_count = 1'000u;
+
+    auto algo = headcode::crypt::Factory::Create("openssl-sha1");
+    ASSERT_NE(algo.get(), nullptr);
+    EXPECT_STREQ(algo->GetDescription().name_.c_str(), "openssl-sha1");
+
+    auto time_start = std::chrono::high_resolution_clock::now();
+    for (std::uint64_t i = 0; i < loop_count; ++i) {
+        algo->Add(IPSUM_LOREM_TEXT);
+    }
+    std::vector<std::byte> result;
+    algo->Finalize(result);
+    headcode::benchmark::Throughput throughput{headcode::benchmark::GetElapsedMicroSeconds(time_start),
+                                               loop_count * IPSUM_LOREM_TEXT.size()};
+
+    std::cout << StreamPerformanceIndicators(throughput, "BenchmarkOPENSSLSHA1::OPENSSLSHA1StdString1000 ");
+
+    auto expected = std::string{"34c85ec6eb126270d7e2121e1cfc5ccbbae2a9b2"};
+    EXPECT_STREQ(headcode::mem::MemoryToHex(result).c_str(), expected.c_str());
+}
+
+
+TEST(BenchmarkOPENSSLSHA1, OPENSSLSHA1CArray1000) {
+
+    auto loop_count = 1'000u;
+
+    auto algo = headcode::crypt::Factory::Create("openssl-sha1");
+    ASSERT_NE(algo.get(), nullptr);
+    EXPECT_STREQ(algo->GetDescription().name_.c_str(), "openssl-sha1");
+
+    auto array = IPSUM_LOREM_TEXT.c_str();
+    auto size = std::strlen(array);
+
+    auto time_start = std::chrono::high_resolution_clock::now();
+    for (std::uint64_t i = 0; i < loop_count; ++i) {
+        algo->Add(array, size);
+    }
+    std::vector<std::byte> result;
+    algo->Finalize(result);
+    headcode::benchmark::Throughput throughput{headcode::benchmark::GetElapsedMicroSeconds(time_start),
+                                               loop_count * size};
+
+    std::cout << StreamPerformanceIndicators(throughput, "BenchmarkOPENSSLSHA1::OPENSSLSHA1CArray1000 ");
+
+    auto expected = std::string{"34c85ec6eb126270d7e2121e1cfc5ccbbae2a9b2"};
+    EXPECT_STREQ(headcode::mem::MemoryToHex(result).c_str(), expected.c_str());
+}
---
+++ test/benchmark/openssl/test_openssl_sha384.cpp
@@ -0,0 +1,76 @@
+/*
+ * This file is part of the headcode.space crypt.
+ *
+ * The 'LICENSE.txt' file in the project root holds the software license.
+ * Copyright (C) 2020 headcode.space
+ * https://www.headcode.space, <info@headcode.space>
+ */
+
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <headcode/benchmark/benchmark.hpp>
+#include <headcode/mem/mem.hpp>
+#include <headcode/crypt/crypt.hpp>
+
+#include <shared/ipsum_lorem.hpp>
+
+
+TEST(BenchmarkOPENSSLSHA384, OPENSSLSHA384StdString1000) {
+
+    auto loop_count = 1'000u;
+
+    auto algo = headcode::crypt::Factory::Create("openssl-sha384");
+    ASSERT_NE(algo.get(), nullptr);
+    EXPECT_STREQ(algo->GetDescription().name_.c_str(), "openssl-sha384");
+
+    auto time_start = std::chrono::high_resolution_clock::now();
+    for (std::uint64_t i = 0; i < loop_count; ++i) {
+        algo->Add(IPSUM_LOREM_TEXT);
+    }
+    std::vector<std::byte> result;
+    algo->Finalize(result);
+    headcode::benchmark::Throughput throughput{headcode::benchmark::GetElapsedMicroSeconds(time_start),
+                                               loop_count * IPSUM_LOREM_TEXT.size()};
+
+    std::cout << StreamPerformanceIndicators(throughput, "BenchmarkOPENSSLSHA384::OPENSSLSHA384StdString1000 ");
+
+    auto expected = std::string{
+            "02d6b6d7afc1d669bd7d53cad25a9433"
+            "823e0354e9aeb258ea066a98d8f32eef"
+            "3f05ac9dc281f2653fa043aa14d25962"};
+    EXPECT_STREQ(headcode::mem::MemoryToHex(result).c_str(), expected.c_str());
+}
+
+
+TEST(BenchmarkOPENSSLSHA384, OPENSSLSHA384CArray1000) {
+
+    auto loop_count = 1'000u;
+
+    auto algo = headcode::crypt::Factory::Create("openssl-sha384");
+    ASSERT_NE(algo.get(), nullptr);
+    EXPECT_STREQ(algo->GetDescription().name_.c_str(), "openssl-sha384");
+
+    auto array = IPSUM_LOREM_TEXT.c_str();
+    auto size = std::strlen(array);
+
+    auto time_start = std::chrono::high_resolution_clock::now();
+    for (std::uint64_t i = 0; i < loop_count; ++i) {
+        algo->Add(array, size);
+    }
+    std::vector<std::byte> result;
+    algo->Finalize(result);
+    headcode::benchmark::Throughput throughput{headcode::benchmark::GetElapsedMicroSeconds(time_start),
+                                               loop_count * size};
+
+    std::cout << StreamPerformanceIndicators(throughput, "BenchmarkOPENSSLSHA384::OPENSSLSHA384CArray1000 ");
+
+    auto expected = std::string{
+            "02d6b6d7afc1d669bd7d53cad25a9433"
+            "823e0354e9aeb258ea066a98d8f32eef"
+            "3f05ac9dc281f2653fa043aa14d25962"};
+    EXPECT_STREQ(headcode::mem::MemoryToHex(result).c_str(), expected.c_str());
+}
