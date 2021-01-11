9b380715e64d632f7f9cf17a897114217306f68a
Oliver Maurhart <oliver.maurhart@headcode.space>
2021-01-11 09:23:11 +0100

    Adds missing tiger192 files

---
+++ src/lib/hash/ltc/ltc_tiger192.cpp
@@ -0,0 +1,90 @@
+/*
+ * This file is part of the headcode.space crypt.
+ *
+ * The 'LICENSE.txt' file in the project root holds the software license.
+ * Copyright (C) 2020 headcode.space
+ * https://www.headcode.space, <info@headcode.space>
+ */
+
+#include <headcode/crypt/factory.hpp>
+
+#include <tomcrypt.h>
+
+#include "ltc_tiger192.hpp"
+
+
+using namespace headcode::crypt;
+
+
+/**
+ * @brief   The LibTomCrypt TIGER192 algorithm description.
+ * @return  The description of this algorithm.
+ */
+static Algorithm::Description const & GetDescription() {
+
+    static Algorithm::Description description = {
+            "ltc-tiger192",                                   // name
+            Family::HASH,                                // family
+            {0ul, "Not needed.", false},                 // initial key
+            {0ul, "Not needed.", false},                 // final key
+            "LibTomCrypt TIGER192.",                          // description
+            std::string{"libtomcrypt v"} + SCRYPT        // provider
+    };
+
+    return description;
+}
+
+
+/**
+ * @brief   Produces instances of the algorithm.
+ */
+class LTCTIGER192Producer : public Factory::Producer {
+public:
+    /**
+     * @brief   Call operator - creates the algorithm.
+     * @return  A new algorithm instance.
+     */
+    std::unique_ptr<Algorithm> operator()() const override {
+        return std::make_unique<LTCTIGER192>();
+    }
+
+    /**
+     * @brief   Gets the algorithm description.
+     * @return  A structure describing the algorithm.
+     */
+    Algorithm::Description const & GetDescription() const override {
+        return ::GetDescription();
+    }
+};
+
+
+LTCTIGER192::LTCTIGER192() {
+    tiger_init(&GetState());
+}
+
+
+int LTCTIGER192::Add_(char const * data, std::uint64_t size) {
+    return tiger_process(&GetState(), reinterpret_cast<const unsigned char *>(data), size);
+}
+
+
+int LTCTIGER192::Finalize_(std::vector<std::byte> & result, char const *, std::uint64_t) {
+    result.resize(tiger_desc.hashsize);
+    return tiger_done(&GetState(), reinterpret_cast<unsigned char *>(result.data()));
+}
+
+
+Algorithm::Description const & LTCTIGER192::GetDescription_() const {
+    return ::GetDescription();
+}
+
+
+int LTCTIGER192::Initialize_(char const *, std::uint64_t) {
+    return tiger_init(&GetState());
+}
+
+
+void LTCTIGER192::Register() {
+    auto const & description = ::GetDescription();
+    Factory::Register(description.name_, description.family_, std::make_shared<LTCTIGER192Producer>());
+}
---
+++ test/unit/hash/ltc/test_ltc_tiger192.cpp
@@ -0,0 +1,100 @@
+/*
+ * This file is part of the headcode.space crypt.
+ *
+ * The 'LICENSE.txt' file in the project root holds the software license.
+ * Copyright (C) 2020 headcode.space
+ * https://www.headcode.space, <info@headcode.space>
+ */
+
+#include <gtest/gtest.h>
+
+#include <headcode/crypt/crypt.hpp>
+#include <headcode/mem/mem.hpp>
+
+#include "shared/ipsum_lorem.hpp"
+
+
+TEST(Hash_LTCTIGER192, creation) {
+
+    auto algo = headcode::crypt::Factory::Create("ltc-tiger192");
+    ASSERT_NE(algo.get(), nullptr);
+    EXPECT_EQ(algo->Initialize(), 0);
+
+    headcode::crypt::Algorithm::Description const & description = algo->GetDescription();
+
+    EXPECT_STREQ(description.name_.c_str(), "ltc-tiger192");
+    EXPECT_EQ(description.family_, headcode::crypt::Family::HASH);
+    EXPECT_FALSE(description.description_.empty());
+
+    EXPECT_FALSE(description.final_argument_.needed_);
+    EXPECT_FALSE(description.initial_argument_.needed_);
+}
+
+
+TEST(Hash_LTCTIGER192, simple) {
+
+    auto algo = headcode::crypt::Factory::Create("ltc-tiger192");
+    ASSERT_NE(algo.get(), nullptr);
+
+    auto text = std::string{"The quick brown fox jumps over the lazy dog."};
+    EXPECT_EQ(algo->Add(text), 0);
+    std::vector<std::byte> hash;
+    EXPECT_EQ(algo->Finalize(hash), 0);
+    EXPECT_EQ(hash.size(), 24ul);
+    auto expected = std::string{"0bf46f237681b35301d46aa08d43c449643408521a263929"};
+    EXPECT_STREQ(headcode::mem::MemoryToHex(hash).c_str(), expected.c_str());
+}
+
+
+TEST(Hash_LTCTIGER192, regular) {
+
+    auto algo = headcode::crypt::Factory::Create("ltc-tiger192");
+    ASSERT_NE(algo.get(), nullptr);
+    ASSERT_STREQ(algo->GetDescription().name_.c_str(), "ltc-tiger192");
+    EXPECT_EQ(algo->Initialize(), 0);
+    EXPECT_TRUE(algo->IsInitialized());
+    EXPECT_FALSE(algo->IsFinalized());
+
+    algo->Add(IPSUM_LOREM_TEXT);
+    std::vector<std::byte> hash;
+    EXPECT_EQ(algo->Finalize(hash), 0);
+    EXPECT_EQ(hash.size(), 24ul);
+    auto expected = std::string{"eafd96d4d67afa8a8b2d4c703afc3e7064e0fc711c459003"};
+    auto result = headcode::mem::MemoryToHex(hash);
+    EXPECT_STREQ(headcode::mem::MemoryToHex(hash).c_str(), expected.c_str());
+
+    EXPECT_TRUE(algo->IsFinalized());
+}
+
+
+TEST(Hash_LTCTIGER192, empty) {
+
+    auto algo = headcode::crypt::Factory::Create("ltc-tiger192");
+    ASSERT_NE(algo.get(), nullptr);
+    ASSERT_STREQ(algo->GetDescription().name_.c_str(), "ltc-tiger192");
+    EXPECT_EQ(algo->Initialize(), 0);
+
+    std::vector<std::byte> hash;
+    EXPECT_EQ(algo->Finalize(hash), 0);
+    EXPECT_EQ(hash.size(), 24ul);
+    auto expected = std::string{"3293ac630c13f0245f92bbb1766e16167a4e58492dde73f3"};
+    EXPECT_STREQ(headcode::mem::MemoryToHex(hash).c_str(), expected.c_str());
+}
+
+
+TEST(Hash_LTCTIGER192, noinit) {
+
+    auto algo = headcode::crypt::Factory::Create("ltc-tiger192");
+    ASSERT_NE(algo.get(), nullptr);
+    ASSERT_STREQ(algo->GetDescription().name_.c_str(), "ltc-tiger192");
+
+    algo->Add(IPSUM_LOREM_TEXT);
+    std::vector<std::byte> hash;
+    EXPECT_EQ(algo->Finalize(hash), 0);
+    EXPECT_EQ(hash.size(), 24ul);
+    auto expected = std::string{"eafd96d4d67afa8a8b2d4c703afc3e7064e0fc711c459003"};
+    EXPECT_STREQ(headcode::mem::MemoryToHex(hash).c_str(), expected.c_str());
+
+    EXPECT_FALSE(algo->IsInitialized());
+    EXPECT_TRUE(algo->IsFinalized());
+}
